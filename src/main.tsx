89b96c9b383c46c404faa4c8f0630cc9988d41ea
purplevarun <purplevarun@gmail.com>
2024-02-02 12:55:05 +0530

    project initialization

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+const container = document.getElementById("root")!;
+ReactDOM.createRoot(container).render(<App />);
