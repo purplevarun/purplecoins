2459de7025882d166262c5d1053cc369768c289a
purplevarun <purplevarun@gmail.com>
2024-08-27 19:39:53 +0530

    Hello World

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<StatusBar style="light" />
+			<Text style={styles.text}>Hello World</Text>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#15151c",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	text: {
+		fontSize: 40,
+		color: "#9899a7",
+	},
 });
+
+export default App;
